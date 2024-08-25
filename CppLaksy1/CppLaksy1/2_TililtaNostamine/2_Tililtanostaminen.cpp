ee55ecf16becf1592711cd5e48e31d9b851f8dc3
Jari Riihimäki <riihimaki.jari@hotmail.fi>
2024-08-25 22:11:46 +0300

    Tehtävä 2 korjaus

---
+++ CppLaksy1/CppLaksy1/2_TililtaNostamine/2_Tililtanostaminen.cpp
@@ -0,0 +1,42 @@
+﻿#include "2_Tililtanostaminen.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+
+void teeTalletus(PankkiTili& tili, int maara, int toistot) {
+    for (int i = 0; i < toistot; i++) {
+        tili.tallenna(maara);
+    }
+}
+
+void teeNosto(PankkiTili& tili, int maara, int toistot) {
+    for (int i = 0; i < toistot; i++) {
+        tili.nosta(maara);
+    }
+}
+
+void tililtaNostaminenMain() {
+    PankkiTili tili;
+    const int siirrot = 100;
+    const int maara = 1;
+
+    thread t1(teeTalletus, ref(tili), maara, siirrot);
+    thread t2(teeNosto, ref(tili), maara, siirrot);
+
+    t1.join();
+    t2.join();
+
+    cout << "Lopullinen saldo: " << tili.haeSaldo() << endl;
+    cout << "Talletuksia: " << tili.getTalletusCount() << endl;
+    cout << "Nostoja: " << tili.getNostoCount() << endl;
+
+    if (tili.haeSaldo() > 0) {
+        cout << "Soitetaan asiakaspalveluun" << endl;
+        cout << "Odotetaan 3 pankkipäivää" << endl;
+        tili.resetSaldo();
+        cout << "Saldo on nyt " << tili.haeSaldo() << endl;
+    }
+}
---
+++ CppLaksy1/CppLaksy1/2_TililtaNostamine/2_Tililtanostaminen.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <mutex>
+
+class PankkiTili {
+public:
+    PankkiTili() : saldo(0), talletus_count(0), nosto_count(0) {}
+
+    void tallenna(int maara) {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        saldo += maara;
+        talletus_count++;
+    }
+
+    void nosta(int maara) {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        if (saldo >= maara) {
+            saldo -= maara;
+            nosto_count++;
+        }
+    }
+
+    int haeSaldo() const {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        return saldo;
+    }
+
+    int getTalletusCount() const {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        return talletus_count;
+    }
+
+    int getNostoCount() const {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        return nosto_count;
+    }
+
+    void resetSaldo() {
+        std::lock_guard<std::mutex> guard(tili_mutex);
+        saldo = 0;
+    }
+
+private:
+    int saldo;
+    int talletus_count;
+    int nosto_count;
+    mutable std::mutex tili_mutex;
+};
