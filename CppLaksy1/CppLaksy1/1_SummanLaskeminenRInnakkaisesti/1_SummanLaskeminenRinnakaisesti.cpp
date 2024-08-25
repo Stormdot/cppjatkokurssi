a10ea6f34822301f229ed7617b884533bf91c010
Jari Riihimäki <riihimaki.jari@hotmail.fi>
2024-08-25 22:06:47 +0300

    Tehtävät 1 & 2

---
+++ CppLaksy1/CppLaksy1/1_SummanLaskeminenRInnakkaisesti/1_SummanLaskeminenRinnakaisesti.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <numeric>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+const size_t koko = 10000000;
+const int saikeidenMaara = 4;
+vector<size_t> taulukko(koko);
+mutex lukko;
+
+void laskeSumma(size_t aloitusIndeksi, size_t lopetusIndeksi, long long* summa) {
+    long long localSum = accumulate(taulukko.begin() + aloitusIndeksi, taulukko.begin() + lopetusIndeksi, 0LL);
+    lock_guard<mutex> guard(lukko);
+    *summa = localSum;
+    cout << "Thread: " << this_thread::get_id() << " laskee summan indekseilla " << aloitusIndeksi << " - " << lopetusIndeksi << "\n Summa on: " << *summa << endl;
+}
+
+int main() {
+    iota(taulukko.begin(), taulukko.end(), 1);
+    vector<thread> saikeet;
+    vector<long long> summat(saikeidenMaara, 0);
+
+    size_t osanKoko = koko / saikeidenMaara;
+    for (int i = 0; i < saikeidenMaara; ++i) {
+        size_t aloitusIndeksi = i * osanKoko;
+        size_t lopetusIndeksi = (i == saikeidenMaara - 1) ? koko : aloitusIndeksi + osanKoko;
+        saikeet.push_back(thread(laskeSumma, aloitusIndeksi, lopetusIndeksi, &summat[i]));
+    }
+
+    for (auto& saie : saikeet) {
+        saie.join();
+    }
+
+    long long kokonaissumma = accumulate(summat.begin(), summat.end(), 0LL);
+
+    cout << "Kokonaissumma: " << kokonaissumma << endl;
+
+    return 0;
+}
